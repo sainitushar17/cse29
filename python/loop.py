9e9f6d29b4afdc8d4369801df7c430806c404e8a
sainitushar17 <tushar.ts.saini@gmail.com>
2025-09-19 11:49:44 +0530

    initial commit

--- python/loop.py
+++ python/loop.py
@@ -1,2 +1,9 @@
+age = int(input("Enter your age: "))
 
+if age < 0:
+    print("Age could not be less than 0")
+elif age < 18:
+    print("You are less than 18")
+else:
+    print("You are above 18")
     
